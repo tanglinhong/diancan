97f1feaba0773160d9785f6578e277fe250296cc
Grinch <majorgrinch@gmail.com>
2016-09-28 17:23:42 +0800

    DIY user

--- login/models.py
+++ login/models.py
@@ -6,5 +6,14 @@ class MyUser(AbstractBaseUser):
 	email = models.CharField(max_length=40)
 	cellphone = models.CharField(max_length=11)
 	USERNAME_FIELD = 'username'
+	REQUIRED_FIELDS = ['email',]
+	is_active = models.BooleanField(default=True)
+
+	def get_full_name():
+		return username
+
+	def get_short_name():
+		return username
+
 	class Meta:
 		db_table = 'MyUser'
