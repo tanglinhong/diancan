667482333bff856c3eda46513b1afe24fe170ed0
Grinch <majorgrinch@gmail.com>
2016-10-05 02:47:50 +0800

    Found that database does not support chinese'

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -7,4 +7,5 @@ app_name = 'mainpage'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^account$', views.my_account, name='my_account'),
+    url(r'^complete_info$', views.complete_info, name='complete'),
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -3,7 +3,8 @@ from django.template import loader
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from login.models import User
-
+from .models import Address
+from os import sys
 
 def index(request):
 	print(request.user.username=='')
@@ -12,3 +13,34 @@ def index(request):
 
 def my_account(request):
 	return render(request, 'mainpage/account.html', {'user': request.user})
+
+def complete_info(request):
+	curr_user = User.objects.get(pk=request.user.id)
+	try:
+		province = request.POST['province']
+		city = request.POST['city']
+		county = request.POST['county']
+		street = request.POST['street']
+		consignee = request.POST['consignee']
+		consignee_tel = request.POST['consignphone']
+		moren = request.POST['moren']
+		email = request.POST['email']
+		if moren is 'true':
+			is_default = True
+		else:
+			is_default = False
+		cellphone = request.POST['phone']
+		curr_user.cellphone = cellphone
+		curr_user.email = email
+		curr_user.save()
+		if province!='':
+			address = [province, city, county, street, consignee, consignee_tel]
+			print(address)
+			addr = Address(province='province', city=city, county=county, street=street,
+				consignee=consignee, consignee_tel=consignee_tel, user_id=curr_user)
+			addr.save()
+		return HttpResponse(1)
+	except:
+		import traceback
+		print(traceback.format_exc())
+		return HttpResponse(0)
