ba0b045cac649bd4fdbf004088c638a18ef76d97
tanglinhong <764424892@qq.com>
2016-10-13 12:22:23 +0800

    add orders_list to personal-center page

--- diancan/settings.py
+++ diancan/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'diancan_db',
         'USER': 'root',
-        'PASSWORD':'toor',
+        'PASSWORD':'tlh5899289',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
