d69b299b3f899bc0d8d085bfaca9883b023d7d34
Grinch <majorgrinch@gmail.com>
2016-10-14 01:12:51 +0800

    Update shoppingcar.js

--- mainpage/static/mainpage/js/shoppingcar.js
+++ mainpage/static/mainpage/js/shoppingcar.js
@@ -21,23 +21,6 @@ function recalculateTotalPrice($table){
 
 function initialTable(){
     //---从cache中加载数据，然后初始化所有表格；
-    // var obj=JSON.parse(data);
-    // for(var i in obj){//处理每一个表格中，
-    //     var shopId=i;
-    //     console.log("商店id:"+shopId);
-    //     var innerObj=obj[i];
-    //     $('<table id="order" data-shop-id='+shopId+'>')
-    //     for(var j in innerObj){
-    //         var foodId=j;
-    //         var foodCount=innerObj[j];
-    //         console("商品ID："+foodId+" 商品数量："+foodCount);
-
-
-    //     }
-
-
-
-    // }
 
 
     //计算每个table的价格
